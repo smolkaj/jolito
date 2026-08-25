fb3873003d450e5720504880e9795e93dd58b68e
Steffen Smolka <steffen.smolka@gmail.com>
2026-08-25 08:32:56 -0700

    chore(pwa): bump service worker cache version to jolito-shell-v2

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /* global self, caches, fetch, URL */
 
-const CACHE_NAME = 'jolito-shell-v1'
+const CACHE_NAME = 'jolito-shell-v2'
 const scopePath = new URL(self.registration.scope).pathname
 const shellUrl = scopePath
 const indexUrl = `${scopePath}index.html`
