f2dbb879962339f896cbcea9479607c1409e7679
Steffen Smolka <steffen.smolka@gmail.com>
2026-08-25 13:23:25 -0700

    perf(dev): optimize developer feedback loop, caching, and test execution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import { searchForWorkspaceRoot } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), '..'],
+    },
+  },
   test: {
     environment: 'jsdom',
     include: ['src/**/*.test.{ts,tsx}'],
